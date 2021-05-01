df949c673ac8c35863b4f255ba4f49d5e79db433
Nikolaos Papadakis <nipapadak@gmail.com>
2021-05-01 08:42:29 -0400

    expose Auth challenge callback (+2 squashed commits) Squashed commits: [771f5b3d9] add @class definition for Fetcher [6a07a3373] Functions expose GTMSessionFetcher

--- Functions/FirebaseFunctions/FIRFunctions.m
+++ Functions/FirebaseFunctions/FIRFunctions.m
@@ -330,6 +330,12 @@ NSString *const kFUNDefaultRegion = @"us-central1";
   return _emulatorOrigin;
 }
 
+- (void)setFetcherServiceChallengeBlock:(AuthChallengeBlock)challengeBlock {
+    _fetcherService.challengeBlock = ^(GTMSessionFetcher *fetcher, NSURLAuthenticationChallenge *challenge, GTMSessionFetcherChallengeDispositionBlock dispositionBlock) {
+                                                  challengeBlock(challenge, dispositionBlock);
+                                      };
+}
+
 @end
 
 NS_ASSUME_NONNULL_END
